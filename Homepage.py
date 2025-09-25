e05dae0edc92d8c44cd24a0a3f6dd4ea4f61a6aa
basstyy <bastian.armijo@estudiantes.colegiotgs.cl>
2025-09-25 15:18:17 -0300

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # ---------------------  Contenido del sitio   --------------------------#
 # Sección Inicial
-st.title("¡Bienvenidos a la Región de Teselia! Un Viaje Inolvidable")
+st.title("¡Bienvenidos a la Región de Teselia holamister gonazalo! Un Viaje Inolvidable")
 st.badge("Introducción", color="green", icon=":material/chevron_right:")
 
 # Sutítulo
@@ -18,5 +18,6 @@ Aquí, exploraremos cada rincón de esta increíble región. Hablaremos en detal
 Pero no nos detendremos ahí. Explicaremos por qué, para muchos, Teselia es considerada la mejor región de Pokémon. Desde su innovadora narrativa y su intrigante trama centrada en el Equipo Plasma y los legendarios Reshiram y Zekrom, hasta la gran diversidad de Pokémon que se pueden encontrar. Teselia fue la primera región en introducir 156 Pokémon completamente nuevos, dándonos una experiencia de descubrimiento fresca y emocionante sin igual.
 
 Además, nos adentraremos en las historias de los amigos de Ash Ketchum en esta temporada: la brillante y talentosa Cilan, un líder de gimnasio y conocedor Pokémon, y la valiente y decidida Iris, quien sueña con ser una Maestra Dragón. Su viaje juntos, lleno de risas, desafíos y valiosas lecciones, es una parte fundamental de la experiencia de Teselia.
+
 
 ¡Prepárense para revivir viejos recuerdos y descubrir nuevos secretos de una de las regiones más queridas de todo el universo Pokémon! ¡Comienza tu viaje a Teselia ahora mismo! """)
